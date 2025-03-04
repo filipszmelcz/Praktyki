c9dfa9166c1adf687fda4ae68c7940cd236e068f
filipszmelcz <filip.szmelcz@gmail.com>
2025-03-04 15:59:48 +0100

    change 2

--- space_travel_calculator.py
+++ space_travel_calculator.py
@@ -1,14 +1,4 @@
 from search_engine_for_planet_info import solar_system
-# solar_system = {
-#     "Merkury": 57900000,
-#     "Wenus": 108200000,
-#     "Ziemia": 149600000,
-#     "Mars": 227900000,
-#     "Jowisz": 778300000, 
-#     "Saturn":1427000000,
-#     "Uran": 2871000000,
-#     "Neptun": 4498300000,
-# }
 
 transport = {
     "Prędkość światła" : 1079252849,
@@ -25,13 +15,4 @@ def calculator():
     t = int(s/v)
     print(f"Czas podróży: {t} dni")
 calculator()
-    # if solar_system[dest] - solar_system[start] > 0:
-    #     s = solar_system[dest] - solar_system[start]
-    #     v = transport[trans]
-    #     t = int(s/v)
-    #     print(f"Czas podróży: {t} dni")
-    # else:
-    #     s = solar_system[start] - solar_system[dest]
-    #     v = transport[trans]
-    #     t = int(s/v)
-    # print(f"Czas podróży: {t} dni")
+
