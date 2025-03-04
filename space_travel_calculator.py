05c16d6ff35a27692dd8efed22007acb78da34cb
filipszmelcz <filip.szmelcz@gmail.com>
2025-03-04 15:59:00 +0100

    change 2

--- space_travel_calculator.py
+++ space_travel_calculator.py
@@ -1,13 +1,14 @@
-solar_system = {
-    "Merkury": 57900000,
-    "Wenus": 108200000,
-    "Ziemia": 149600000,
-    "Mars": 227900000,
-    "Jowisz": 778300000, 
-    "Saturn":1427000000,
-    "Uran": 2871000000,
-    "Neptun": 4498300000,
-}
+from search_engine_for_planet_info import solar_system
+# solar_system = {
+#     "Merkury": 57900000,
+#     "Wenus": 108200000,
+#     "Ziemia": 149600000,
+#     "Mars": 227900000,
+#     "Jowisz": 778300000, 
+#     "Saturn":1427000000,
+#     "Uran": 2871000000,
+#     "Neptun": 4498300000,
+# }
 
 transport = {
     "Prędkość światła" : 1079252849,
@@ -19,15 +20,18 @@ def calculator():
     start = input("Podaj planetę startową: ")
     dest = input("Podaj planetę docelową: ")
     trans = input("Podaj sposób transportu (Prędkość światła, Voyager 1, Falcon 9): ")
-    if solar_system[dest] - solar_system[start] > 0:
-        s = solar_system[dest] - solar_system[start]
-        v = transport[trans]
-        t = int(s/v)
-        print(f"Czas podróży: {t} dni")
-    else:
-        s = solar_system[start] - solar_system[dest]
-        v = transport[trans]
-        t = int(s/v)
-        
-        print(f"Czas podróży: {t} dni")
+    v = transport.get(trans)
+    s = abs(int(solar_system.get(dest).get("Odległośc od Słońca(km): ")) - int(solar_system.get(start).get("Odległośc od Słońca(km): ")))
+    t = int(s/v)
+    print(f"Czas podróży: {t} dni")
 calculator()
+    # if solar_system[dest] - solar_system[start] > 0:
+    #     s = solar_system[dest] - solar_system[start]
+    #     v = transport[trans]
+    #     t = int(s/v)
+    #     print(f"Czas podróży: {t} dni")
+    # else:
+    #     s = solar_system[start] - solar_system[dest]
+    #     v = transport[trans]
+    #     t = int(s/v)
+    # print(f"Czas podróży: {t} dni")
